b8eb91d89b0707eb6c8e93d6375df725766a5e17
Vinicius <boringmkt@gmail.com>
2024-12-16 10:56:09 -0300

    :tada: Inicio projeto spring

---
+++ src/main/java/com/generation/farmacia/ProjetoFinalBloco02Application.java
@@ -0,0 +1,13 @@
+package com.generation.farmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalBloco02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalBloco02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/farmacia/ProjetoFinalBloco02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalBloco02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
